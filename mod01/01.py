a248688b97855b605dfef0e16533eece3912037c
eetua <elsavii@metropolia.fi>
2025-08-19 15:22:05 +0300

    init

---
+++ mod01/01.py
@@ -0,0 +1,15 @@
+# moikka olen kommentti
+'''moikka olen kommentti'''
+
+print('Hello world')
+print('just starting')
+print("hello world")
+print('"hello hell"')
+
+# yksinkertainen muuttuja
+nimi='Eetu'
+print(nimi)
+
+#syöte, eli tietoja käyttäjältä
+nimi2=input('moikka, anna nimesi: ')
+print(nimi2)
