a892b292b163189e1adb002f713bd2f00602f0f1
eetua <elsavii@metropolia.fi>
2025-09-14 21:21:11 +0300

    init

--- mod4/tehtava2.py
+++ mod4/tehtava2.py
@@ -3,3 +3,16 @@ while tuumat > 0:
     sentit = tuumat * 2,54
     print(sentit)
     tuumat = int(input("Anna tuumat: "))
+
+
+#tai
+ohjelma_paalla = True
+while ohjelma_paalla == True:
+
+    tuumat = int(input("Anna tuumat: "))
+    if tuumat < 0:
+        ohjelma_paalla = False
+    else:
+        sentit = tuumat * 2,54
+        print(sentit)
+
