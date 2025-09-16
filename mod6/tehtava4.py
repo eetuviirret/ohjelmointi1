303650709c884ecf328c28393ef98f28841c01f2
eetua <elsavii@metropolia.fi>
2025-09-16 18:30:01 +0300

    init

---
+++ mod6/tehtava4.py
@@ -0,0 +1,13 @@
+'''Kirjoita funktio, joka saa parametrinaan listan
+kokonaislukuja. Ohjelma palauttaa listassa olevien
+lukujen summan. Kirjoita testausta varten pääohjelma,
+jossa luot listan, kutsut funktiota ja tulostat sen
+palauttaman summan.'''
+
+luvut = [9, 8, 7, 6, 5, 4, 3, 2]
+
+def summa(luvut):
+    print(sum(luvut))
+    return
+
+summa(luvut)
