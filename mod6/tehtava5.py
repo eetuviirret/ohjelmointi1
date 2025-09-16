b93e4eb54d46fbdee422515c43794191509ec02d
eetua <elsavii@metropolia.fi>
2025-09-16 19:17:31 +0300

    init

--- mod6/tehtava5.py
+++ mod6/tehtava5.py
@@ -11,10 +11,46 @@ saatu lista paitsi että siitä on karsittu pois kaikki parittomat luvut.
 Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota
 ja tulostat sen jälkeen sekä alkuperäisen että karsitun listan.'''
 
+'''luvut = [8,4,5,3,5,1,7,9] 
+
+def parilliset_luvut(luvut): 
+    for l in luvut: #käy lista läpi 
+        print(l) 
+        if l//2 != 0 and l%2 != 0: # jos tämä 
+            luvut.remove(l) 
+            print("l removed") 
+    print(luvut) 
+    return 
+
+parilliset_luvut(luvut)
+
+Yritetty selvittää miksi ei toimi, kysytään chatgbt:ltä
+
+Vastaus:
+Ongelma on siinä, että muokkaat listaa samalla
+kun iteroit sen yli. Tämä johtaa siihen,
+että osa arvoista jää tarkistamatta,
+koska kun poistat alkion listasta, indeksit siirtyvät
+– mutta for-silmukka ei ota tätä huomioon.
+
+Luo uusi lista joka sisältää vain parilliset luvut
+
+luvut = [8, 4, 5, 3, 5, 1, 7, 9]
+
+def parilliset_luvut(luvut):
+    parilliset = [l for l in luvut if l % 2 == 0]
+    print(parilliset)
+    return parilliset
+
+parilliset_luvut(luvut)
+
+Oma luettavampi versio josta saan selvää:
+'''
+
 luvut = [8,4,5,3,5,1,7,9]
 
 def parilliset_luvut(luvut):
-    parilliset = []
+    parilliset = [] #pitää tehdä oma lista parillisille kos
     for l in luvut: #käy lista läpi
         if l%2 == 0: # jos tämä
             parilliset.append(l)
