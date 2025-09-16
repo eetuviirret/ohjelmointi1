4ef22aa7e1999538cd6e42248f92d277362a81e4
eetua <elsavii@metropolia.fi>
2025-09-16 16:42:51 +0300

    init

--- mod4/tehtava5.py
+++ mod4/tehtava5.py
@@ -1,5 +1,17 @@
-kayttajatunnus = "python"
-salasana = "rules"
+
 i = 0
-while i < 5:
-    kayttajatunnus
+ohjelma_paalla = True
+
+while ohjelma_paalla:
+    if i < 5:
+        kayttajatunnus_syote = input("Anna käyttäjätunnus:")
+        salasana_syote = input("Anna salasana:")
+        if kayttajatunnus_syote == "python" and salasana_syote == "rules":
+            print("Tervetuloa")
+            ohjelma_paalla = False
+        else:
+            print("Pääsy evätty")
+            i = i + 1
+    if i == 5:
+        ohjelma_paalla = False
+        print("Pääsy evätty 5 kertaa. Ohjelma sammutetaan.")
