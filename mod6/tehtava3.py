cb6f45823ee698b14b6b6a6bf9176d16ac76f6a7
eetua <elsavii@metropolia.fi>
2025-09-16 18:10:41 +0300

    init

---
+++ mod6/tehtava3.py
@@ -0,0 +1,17 @@
+#fuktiot
+
+'''Kirjoita funktio, joka saa parametrinaan bensiinin määrän
+Yhdysvaltain nestegallonoina ja palauttaa paluuarvonaan vastaavan litramäärän.
+Kirjoita pääohjelma, joka kysyy gallonamäärän käyttäjältä
+ja muuntaa sen litroiksi. Muunnos on tehtävä aliohjelmaa hyödyntäen.
+Muuntamista jatketaan siihen saakka, kunnes käyttäjä syöttää negatiivisen gallonamäärän.
+Yksi gallona on 3,785 litraa.'''
+
+def usa_gal_to_l(gal):
+    litre = gal*3.78541
+    return litre
+
+gal = int(input("Anna gallonit: "))
+while gal >= 0:
+    gal = int(input("Anna gallonit: "))
+    print(usa_gal_to_l(gal))
