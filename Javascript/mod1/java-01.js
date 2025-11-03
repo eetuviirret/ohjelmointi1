ff802246b5435fa3de576f01ec41ae828259c5d1
eetuviirret <elsavii@metropolia.fi>
2025-11-03 11:54:23 +0200

    init

---
+++ Javascript/mod1/java-01.js
@@ -0,0 +1,9 @@
+'use strict';
+  //käytä const avainsanaa muuttujan esittelyyn
+  //paitsi jos sen arvoa tarvitsee muuttaa
+/*
+monirivinen kommentti
+ */
+  const teksti = 'hello world'
+  console.log('This is text is printed to the console.');
+  console.log(teksti)
