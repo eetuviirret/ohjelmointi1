db7ccc038ef97e60b43b1ae1b084724886ea103f
eetuviirret <elsavii@metropolia.fi>
2025-11-05 11:01:30 +0200

    init

--- Javascript/mod1/tehtava1.js
+++ Javascript/mod1/tehtava1.js
@@ -1,1 +1,3 @@
-console.log{"I am printing to console!"}
+//Write a program that logs to the console this text:
+// I'm printing to console! (1p)
+console.log('I am printing to console!');
---
+++ Javascript/mod1/tehtava2.js
@@ -0,0 +1,4 @@
+//Write a program that prompts for user's name and then greets the user.
+// Print the result to the HTML document: Hello, Name! (2p)
+
+let name = prompt('type your name:');
