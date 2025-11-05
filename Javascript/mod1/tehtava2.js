8f7f819577a604da47b4a6f44122526d28e59713
eetuviirret <elsavii@metropolia.fi>
2025-11-05 11:42:50 +0200

    init

--- Javascript/mod1/tehtava2.js
+++ Javascript/mod1/tehtava2.js
@@ -1,4 +1,5 @@
 //Write a program that prompts for user's name and then greets the user.
 // Print the result to the HTML document: Hello, Name! (2p)
-
-let name = prompt('type your name:');
+'use strict';
+let name = prompt('type your name: ');
+document.querySelector('#target').innerHTML = 'Hello ' + name + '!';
---
+++ Javascript/mod1/tehtava3.js
@@ -0,0 +1,9 @@
+//Write a program that prompts for three integers.
+// The program prints the sum, product and average of the numbers
+// to the HTML document. (3p)
+//remember to convert strings to numbers when adding them together.
+'use strict';
+ document.querySelector('#sum').innerHTML = 'Sum: ' + s;
+ document.querySelector('#product').innerHTML = 'product: ' + p;
+ document.querySelector('#average').innerHTML = 'average: ' + a;
+
