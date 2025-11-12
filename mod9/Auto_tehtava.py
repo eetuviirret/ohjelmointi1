48d02a526ba049ea3198af405e343a2f45d0013b
eetuviirret <elsavii@metropolia.fi>
2025-11-12 09:23:25 +0200

    Mod 10, tehtava 4

--- mod9/Auto_tehtava.py
+++ mod9/Auto_tehtava.py
@@ -25,6 +25,14 @@ class Car:
     def car_run(self, time):
         self.trip = self.trip + self.speed * time
 
+class race:
+    def __init__(self, race_name, length)
+        self.race_name = race_name
+        self.length = length
+        self.car_list = []
+
+    def hour_pass(self):
+
 car1 = Car("123", 142)
 
 print(car1.speed)
@@ -106,3 +114,30 @@ for r in race_cars:
 
 
 print(f"Winner is {winner}")
+
+'''Tehtävä on jatkoa aiemmalle autokilpailutehtävälle. 
+Kirjoita Kilpailu-luokka, jolla on ominaisuuksina kilpailun nimi,
+pituus kilometreinä ja osallistuvien autojen lista. Luokassa on alustaja,
+joka saa parametreinaan nimen, kilometrimäärän ja autolistan
+ja asettaa ne ominaisuuksille arvoiksi. Luokassa on seuraavat metodit:
+
+tunti_kuluu, joka toteuttaa aiemmassa autokilpailutehtävässä mainitut
+tunnin välein tehtävät toimenpiteet eli arpoo kunkin auton
+nopeuden muutoksen ja kutsuu kullekin autolle kulje-metodia.
+
+tulosta_tilanne, joka tulostaa kaikkien autojen sen hetkiset
+tiedot selkeäksi taulukoksi muotoiltuna.
+
+kilpailu_ohi, joka palauttaa True, jos jokin autoista on maalissa
+eli se on ajanut vähintään kilpailun kokonaiskilometrimäärän.
+Muussa tapauksessa palautetaan False.
+
+Kirjoita pääohjelma, joka luo 8000 kilometrin kilpailun nimeltä
+"Suuri romuralli". Luotavalle kilpailulle annetaan kymmenen 
+auton lista samaan tapaan kuin aiemmassa tehtävässä.
+Pääohjelma simuloi kilpailun etenemistä kutsumalla toistorakenteessa
+tunti_kuluu-metodia, jonka jälkeen aina tarkistetaan 
+kilpailu_ohi-metodin avulla, onko kilpailu ohi.
+Ajantasainen tilanne tulostetaan tulosta tilanne-metodin
+avulla kymmenen tunnin välein sekä kertaalleen sen jälkeen,
+kun kilpailu on päättynyt.'''
