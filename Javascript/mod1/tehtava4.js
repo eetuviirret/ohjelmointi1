2b45614814d0e5718034f797329098c859b7fda0
eetuviirret <elsavii@metropolia.fi>
2025-11-19 18:01:20 +0200

    innit

--- Javascript/mod1/tehtava4.js
+++ Javascript/mod1/tehtava4.js
@@ -1,6 +1,26 @@
 'use strict'
 
 const name = prompt('Enter your name: ');
-guild = console_log(Math.random(0, 3));
-
-const sorting = 'You are ${}';
+let result = Math.ceil(Math.random()*4);
+switch (result)
+{
+    case 1:
+        console.log('You are... Gryffindor!');
+        document.querySelector('#sorting').innerHTML = 'You are... Gryffindor!';
+        break;
+    case 2:
+        console.log('You are... Slythering!');
+        document.querySelector('#sorting').innerHTML = 'You are... Slythering!';
+        break;
+    case 3:
+        console.log('You are... Rawenclaw!');
+        document.querySelector('#sorting').innerHTML = 'You are... Rawenclaw!';
+        break;
+    case 4:
+        console.log('You are... Hufflepuf!');
+        document.querySelector('#sorting').innerHTML = 'You are... Hufflepuf!';
+        break;
+    default:
+        console.log('You are... Muggle...');
+        document.querySelector('#sorting').innerHTML = 'You are... Muggle...';
+}
