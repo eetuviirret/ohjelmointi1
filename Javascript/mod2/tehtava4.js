43798daabc77347c4e2843433ec254130b6c02e0
eetuviirret <elsavii@metropolia.fi>
2025-12-09 19:29:25 +0200

    init

--- Javascript/mod2/tehtava4.js
+++ Javascript/mod2/tehtava4.js
@@ -1,21 +1,45 @@
-nuberList=[]
+/*Write a program that asks the user for numbers until
+ the user gives zero. The given numbers are printed
+ in the console from the largest to the smallest. (2p)
+ */
+
+/*
+Wanted to practice using switch.
+This is unnessesary complicated and stupid way to do this.
+
+numberList=[]
 let loop = true;
 
-while (loop == true){
-    const answer = prompt("Enter a number:");
+while (loop === true) {
+    const answer = Number(prompt("Enter a number:"));
 
-    switch (answer) {
-        case 'answer != 0':
-            console.log(answer);
-            numberList.push(answer);
+    switch (true) {
+        case isNaN(answer):
+            console.log("invalid answer");
+            alert("Not a number.");
             break;
-        case 'answer = 0':
+        case answer === 0:
             loop = false;
+            break;
+        case answer !== 0:
+            numberList.push(answer);
             break;
         default:
             console.log("invalid answer");
             alert('Not a number.')
             break;
+    }
+}
+numberList.sort((a,b) => b-a);
+console.log(numberList)
+*/
 
-}
-}
+numberList=[]
+let answer = Number(prompt("Enter a number:"));
+
+ while (answer !== 0) {
+     numberList.push(answer);
+     answer = Number(prompt("Enter a number:"));
+ }
+numberList.sort((a,b) => b-a);
+console.log(numberList)
