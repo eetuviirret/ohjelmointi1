fcde5848581d1a11f2a7bef1ecf157f2bf9e70c7
eetuviirret <elsavii@metropolia.fi>
2025-12-12 21:59:06 +0200

    innit

--- Javascript/Mod3/module3-starters-main/t3/3.js
+++ Javascript/Mod3/module3-starters-main/t3/3.js
@@ -1,2 +1,11 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+
+const nameList = document.querySelector('#target');
+let html = '';
+
+    for (let i =0; i < names.length; i++) {
+        html+=`<li>${names[i]}</li>`;
+    }
+
+nameList.innerHTML = html;
